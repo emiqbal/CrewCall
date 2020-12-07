188eade33cfa72b233cd5b9662453328d04f35b9
JToong <toongjosh@gmail.com>
2020-12-07 23:43:26 +0800

    Added more new columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_153701) do
+ActiveRecord::Schema.define(version: 2020_12_07_154205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2020_12_07_153701) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "department"
+    t.boolean "is_producer", default: false
+    t.string "company_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
