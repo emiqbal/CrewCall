8ed58cf6d6af07dd1c37a00616ebde5bf7e007a5
Rupert Yan <yancxr@gmail.com>
2020-12-08 12:37:49 +0800

    starting to create project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,8 +6,19 @@ class ProjectsController < ApplicationController
   end
 
   def new
+  # @producer = current_producer
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+  # @project.producer = current_producer
+    @project.save!
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
   end
 end
