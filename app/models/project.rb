da797ffd1b9ba1c47ffe054ce010759c8da669d2
Rupert Yan <yancxr@gmail.com>
2020-12-08 12:44:34 +0800

    Added photo upload for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,13 @@
 class Project < ApplicationRecord
+  before_destroy :purge_photo
+  has_one_attached :photo
   belongs_to :user
   validates :title, presence: true, length: { maximum: 40 }
   validates :description, presence: true
+
+  private
+
+  def purge_photo
+    photo.purge
+  end
 end
