a2f9c11bb805d7c1f482791c489b1fc2235649e8
iqbal engku <engkuiqbal@outlook.com>
2020-12-09 20:34:29 +0800

    Removed home method from Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :uikit ]
-
-  def home
-  end
-
+  skip_before_action :authenticate_user!, only: [:uikit ]
   def uikit
     @user = current_user
   end
+
+
 end
