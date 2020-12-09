3814ba49b7dcfb90a9c84305927e6bc623ee7ce0
Muhammad Syakiran <syakiranmuhd@gmail.com>
2020-12-09 17:18:46 +0800

    modified the routes to reflect the correct path for jobs projects profiles and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :profiles, except: [:destroy]
+  resources :projects, only: [:index, :show, :new, :create] do
+    resources :jobs, only: [:new, :create]
+  end
+  resources :jobs, only: [:show]
   root to: 'projects#index'
   get '/uikit', to: 'pages#uikit'
-  resources :profiles, only: [:index, :show, :new, :create, :edit, :update]
-  resources :projects, only: [:index, :show, :new, :create] do
-    resources :jobs, only: [:show, :new, :create]
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# users/:id/profiles
