b5c417be79c46d6aca924ebfca9e184ad3e18513
iqbal engku <engkuiqbal@outlook.com>
2020-12-09 21:08:12 +0800

    User Seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Clearing Database...'
+Job.destroy_all
+Project.destroy_all
+User.destroy_all
+
+profile = 'https://www.flaticon.com/svg/static/icons/svg/599/599305.svg'
+people = %w[syakiran iqbal josh rupert grace]
+random_boolean = [true, false].sample
+
+people.each do |person|
+  pic = URI.open(profile)
+  puts "Creating User using seed..."
+  user = User.new(
+    username: "#{person}",
+    email: "#{person}@gmail.com",
+    password: "123456",
+    is_producer: random_boolean
+    )
+
+  user.photo.attach(io: pic, filename: "#{person}.png", content_type: 'image/png')
+  user.save!
+end
+puts "User Seeding done"
+
+
