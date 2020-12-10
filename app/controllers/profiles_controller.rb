50be9614ad060fa13a6fd59818683ad6537b980d
Muhammad Syakiran <syakiranmuhd@gmail.com>
2020-12-10 08:58:31 +0800

    ammended the routes and deleted unnecesary views from profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,28 +8,42 @@ class ProfilesController < ApplicationController
     @profile = Profile.find(params[:id])
   end
 
-  def new
-    @profile = Profile.new
-  end
+  # def new
+  #   @profile = Profile.new
+  # end
 
-  def create
-    @profile = Profile.new(profile_params)
-    @profile.user = current_user
-    if @profile.save
-      redirect_to profile_path(@profile)
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @profile = Profile.new(profile_params)
+  #   @profile.user = current_user
+  #   if @profile.save
+  #     redirect_to profile_path(@profile)
+  #   else
+  #     render :new
+  #   end
+  # end
 
   def edit
-    @profile = Profile.find(params[:id])
+    @profile = Profile.where(user: current_user)
+    @profile = Profile.new if @profile.nil?
   end
 
   def update
-    @profile = Profile.find(params[:id])
-    @profile.update(profile_params)
-    redirect_to profile_path(@profile)
+    @profile = Profile.where(user: current_user)
+    save_status = false
+    if @profile.nil?
+      # profile doesn't exist and must be created
+      @profile = Profile.new(profile_params)
+      @profile.user = current_user
+      save_status = @profile.save
+    else
+      # profile already exists, just update it
+      save_status = @profile.update(profile_params)
+    end
+    if save_status
+      redirect_to profile_path(@profile)
+    else
+      render :edit
+    end
   end
 
   private
