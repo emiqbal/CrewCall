e359ed257e6b18906fb419feaa1a71fdfb0be87a
JToong <toongjosh@gmail.com>
2020-12-11 11:28:18 +0800

    other conflicts

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
 
   validates :bio, :department, :company_name, presence: true
   validates :department, inclusion: { in: %w(Actor Actress Cameraman Lighting_Specialist Video_Editor Script_Writer)}
 
 
-=======
->>>>>>> a19f2a1bdfbc7a6b6c46ada33d3f9e3c2f3db209
 end
