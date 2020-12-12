fdd04978b2b212d04b75dc3e768a61ac756642ca
iqbal engku <engkuiqbal@outlook.com>
2020-12-13 00:31:05 +0800

    Change Department Default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_071957) do
+ActiveRecord::Schema.define(version: 2020_12_12_162755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2020_12_11_071957) do
 
   create_table "profiles", force: :cascade do |t|
     t.text "bio"
-    t.string "department"
+    t.string "department", default: "Production"
     t.string "company_name"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
