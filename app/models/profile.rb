02445e1232345a9050c083dd8ac3abbb3e400399
iqbal engku <engkuiqbal@outlook.com>
2020-12-12 21:24:54 +0800

    Fixed the validation problem

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,12 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  validates :bio, :department, :company_name, presence: true
+  validates :bio, presence: true
+  validates :department, presence: true, unless: :is_producer?
+  validates :company_name, presence: true, if: :is_producer?
   validates :department, inclusion: { in: %w(Actor Actress Cameraman Lighting_Specialist Video_Editor Script_Writer)}
+
+  def is_producer?
+    user.is_producer
+  end
+
 end
