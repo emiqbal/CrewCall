fc5c691c519164848e88d277eb4553fe92fd097f
Muhammad Syakiran <syakiranmuhd@gmail.com>
2020-12-14 16:25:22 +0800

    added validation for username to only one unique word

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
   has_many :user_jobs
   has_many :jobs, through: :user_jobs
   before_destroy :purge_photo
+  validates :username, format: {
+    with: /\A\S+\z/,
+    message: "Please choose a name that is only 1 word."
+  }
 
   private
 
