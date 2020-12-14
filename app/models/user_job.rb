e6e8ff0db88259706c398633fa5028968435684f
Muhammad Syakiran <syakiranmuhd@gmail.com>
2020-12-14 16:07:47 +0800

    added calendar to reflect dates that are with jobs

--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -5,4 +5,10 @@ class UserJob < ApplicationRecord
   def start_time
     self.job.start_date
   end
+
+  def end_time
+    self.job.end_date
+  end
 end
+#user_job.job.project.user
+#user_job.user.username
