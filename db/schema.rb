9754a2dfe9aa57c5212ce308b2a64ef99f21c817
Rupert Yan <yancxr@gmail.com>
2020-12-15 15:12:23 +0800

    jobcard layout proj show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_042216) do
+ActiveRecord::Schema.define(version: 2020_12_15_062428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
