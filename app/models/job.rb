4e46ff2b4bb3f566369699912539a02de029117d
Rupert Yan <yancxr@gmail.com>
2020-12-15 15:12:23 +0800

    jobcard layout proj show page

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,9 +2,9 @@ class Job < ApplicationRecord
   belongs_to :project
   has_many :user_jobs
   has_many :users, through: :user_jobs
+  has_rich_text :rich_description
 
   validates :title, presence: true, length: { maximum: 40 }
-  validates :description, presence: true
   validates :salary, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :department, presence: true, inclusion: { in: %w(Camera Lighting Casting Talent AD Production Transport Location)}
 
